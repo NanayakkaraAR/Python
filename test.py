078d1822f387222b778c2991ef7be72d89320088
NanayakkaraAR <ashinireshana@gmail.com>
2024-09-12 13:32:17 +0530

    edited

---
+++ test.py
@@ -0,0 +1,22 @@
+print("Hello, World!")
+print("I'm Ashini Nanayakkara")
+# people=30
+# cats=20
+# if people < cats:
+#     print("Too many cats!we are doomed!)")
+# else:
+#     print("We can't decide")
+
+# print(2==1)
+# print('string' == "string")
+# print ('string' == "String")
+
+# print (2==1 and True)
+
+# int (2==1 and True)
+
+print(int(2==1 and True))
+
+language_name = "Python"
+print(type(language_name))
+
